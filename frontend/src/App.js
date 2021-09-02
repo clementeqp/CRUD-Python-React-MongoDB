c07cde16f1914897fa69da07169c2b0addad565c
Clementeqp <clementeqp@hotmail.com>
2021-09-02 18:48:48 +0200

    Terminar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,22 @@
-import React from 'react';
-import {BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+import { Navbar } from "./components/Navbar";
+import { About } from "./components/About";
+import { Users } from "./components/Users";
 
 function App() {
   return (
-    <router>
-      <div>
+    <Router>
+      <Navbar />
+
+      <div className="container p-4">
         <Switch>
-          <Route path="/about" component={} />
+          <Route path="/about" component={About} />
+          <Route path="/" component={Users} />
         </Switch>
       </div>
-    </router>
+    </Router>
   );
 }
 
